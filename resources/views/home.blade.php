afced9920657b9224c0795bf5cd68d74399f9928
afianadaaa <afianadaaa18@student.ub.ac.id>
2024-11-15 17:45:48 +0700

    Gambar Buku dan Awan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,15 +19,20 @@
   </section>
 
   <section class="pb-5" style="background-color: #8EACCD;">
-    <div class="container">
+    <div class="container position-relative">
       <div class="row">
-        <div class="col-12 col-md-7 mx-auto">
+        <div class="col-12 col-md-7 mx-auto position-relative">
           <div class="p-4 p-md-5 rounded-9" style="background-color: #295F98;">
             <input type="text" class="form-control rounded-6" style="height: 48px">
             <br>
             <div id="map" class="rounded-6" style="width: 100%; height: 400px;"></div>
           </div>
         </div>
+        <div class="col-12 col-md-8 d-flex justify-content-center align-items-center mx-auto">
+            <img src="/assets/img/buku.png" alt="" style="width: 100%">
+        </div>
+        <img src="/assets/img/awan.png" alt="Awan" class="position-absolute ms-0" style="left: -45%; top: 20%; width:80%";>
+        <img src="/assets/img/awan2.png" alt="Awan" class="position-absolute ms-0" style="right: -45%; top: 20%; width:80%";>
       </div>
     </div>
   </section>
@@ -43,11 +48,11 @@
     }).addTo(map);
 
     // Marker
-    var diy = L.marker([-7.7956, 110.3695]).addTo(map).bindPopup("<b>Yogyakarta</b><br><br><button class='btn btn-sm btn-primary'>Detail</button>");
-    var bali = L.marker([-8.3405, 115.0920]).addTo(map).bindPopup("<b>Bali</b><br><br><button class='btn btn-sm btn-primary'>Detail</button>");
-    var bandung = L.marker([-6.9175, 107.6191]).addTo(map).bindPopup("<b>Bandung</b><br><br><button class='btn btn-sm btn-primary'>Detail</button>");
-    var palembang = L.marker([-2.9909, 104.7563]).addTo(map).bindPopup("<b>Palembang</b><br><br><button class='btn btn-sm btn-primary'>Detail</button>");
-    var makassar = L.marker([-5.1477, 119.4327]).addTo(map).bindPopup("<b>Makassar</b><br><br><button class='btn btn-sm btn-primary'>Detail</button>");
+    var diy = L.marker([-7.7956, 110.3695]).addTo(map).bindPopup("<b>Yogyakarta</b><br><br><button class='btn btn-sm btn-primary'>Pergi</button>");
+    var bali = L.marker([-8.3405, 115.0920]).addTo(map).bindPopup("<b>Bali</b><br><br><button class='btn btn-sm btn-primary'>Pergi</button>");
+    var bandung = L.marker([-6.9175, 107.6191]).addTo(map).bindPopup("<b>Bandung</b><br><br><button class='btn btn-sm btn-primary'>Pergi</button>");
+    var palembang = L.marker([-2.9909, 104.7563]).addTo(map).bindPopup("<b>Palembang</b><br><br><button class='btn btn-sm btn-primary'>Pergi</button>");
+    var makassar = L.marker([-5.1477, 119.4327]).addTo(map).bindPopup("<b>Makassar</b><br><br><button class='btn btn-sm btn-primary'>Pergi</button>");
   
   </script>
   <!-- Jumbotron -->
