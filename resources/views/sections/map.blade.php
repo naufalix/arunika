035d7b6aee7c19386d3022e7d480b96db777d54a
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2024-11-20 12:06:54 +0700

    Add background animation to map section

--- resources/views/sections/map.blade.php
+++ resources/views/sections/map.blade.php
@@ -1,4 +1,48 @@
-<section id="maps" class="pb-5">
+<style>
+  
+</style>
+
+<section id="maps" class="pb-0">
+  <div class="cloud cloud1">
+  <div class="cloud cloud1x">
+  <div class="cloud cloud2">
+  <div class="cloud cloud2x">
+  <div class="cloud cloud3 pb-5">
+    <div class="container aos-init aos-animate" data-aos="fade-up">
+      <div class="section-title aos-init aos-animate" data-aos="zoom-out">
+        <h5 class="text-primary">Jelajahi</h5>
+        <h2 class="h-bg">Peta Interaktif</h2>
+        <p class="col-md-4 mx-auto">
+          <i>Lorem Ipsum...</i>
+        </p>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-10 mx-auto">
+          <div class="card rounded-7" style="background-color: #295F98;">
+            <div class="card-body">
+              <select name="" id="searchcity" class="form-control form-select rounded-6">
+                <option selected disabled value="">Cari Daerah atau Kota disini...</option>
+                @foreach ($cities as $c)
+                  <option value="{{$c->latitude}},{{$c->longitude}}">{{ $c->name }}</option>
+                @endforeach
+              </select>
+              <br>
+              <div id="map" class="rounded-6 mt-3" style="width: 100%; height: 400px;"></div>
+            </div>
+          </div>  
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  </div>
+</section>
+
+{{-- <section id="maps" class="pb-5">
   <div class="container aos-init aos-animate" data-aos="fade-up">
     <div class="section-title text-white aos-init aos-animate" data-aos="zoom-out">
       <h5>Jelajahi</h5>
@@ -25,4 +69,4 @@
 
     </div>
   </div>
-</section>
+</section> --}}
