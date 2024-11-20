3d792e6035acad37b5afbd130878b519f6158711
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2024-11-20 19:19:23 +0700

    Update hero image

--- resources/views/sections/hero.blade.php
+++ resources/views/sections/hero.blade.php
@@ -3,12 +3,12 @@
     <div class="row pt-5">
       <div class="col-12 col-md-6 my-auto py-5">
         <h1 class="">Jelajahi Budaya di Indonesia</h1>
-        <p class="">Platform interaktif untuk menjelajahi keindahan alam, budaya, dan adat Nusantara melalui peta interaktif, ensiklopedia budaya, serta tur virtual VR/AR. Temukan agenda budaya terkini dan ulasan autentik!</p>
+        <p class="">Platform interaktif untuk menjelajahi keindahan alam, budaya, dan adat Nusantara melalui peta interaktif, ensiklopedia budaya, serta tur virtual. Temukan agenda budaya terkini dan ulasan autentik!</p>
         <a class="btn btn-primary shadow-0 rounded-4" href="#maps" >Mulai Eksplorasi <i class="fa fa-sign-out ms-2"></i></a>
       </div>
       <div class="col-12 col-md-6 d-flex">
         <div class="mx-auto">
-          <img src="/assets/img/hero-img.webp" alt="" style="max-width: 100%;max-height: 500px;">
+          <img src="/assets/img/wayang.webp" alt="" style="max-width: 100%;max-height: 500px;">
         </div>
       </div>
     </div>
